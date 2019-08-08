d4e364f2b8e2106c3c5d22b259fbf77176e646da
Sergio Gumiel <sergio.gumiel@hotmail.com>
2019-08-08 20:50:50 +0200

    Initial commit

---
+++ src/main/java/com/cloud/tutorial/apigatewayserver/ApigatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.tutorial.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServerApplication.class, args);
+	}	
+}
